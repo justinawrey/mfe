50272bbef5dd12b47f45e17323ceff2d4c6c7e4c
Justin Awrey <awreyjustin@gmail.com>
2022-05-25 14:43:19 -0700

    added something

--- server.js
+++ server.js
@@ -1,39 +1,22 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { basename, join } from "https://deno.land/std@0.140.0/path/mod.ts";
-
-async function serveFile(path, contentType) {
-  return new Response(await Deno.readFile(path), {
-    headers: {
-      "content-type": contentType,
-    },
-  });
-}
-
-async function serveHtml(path) {
-  return serveFile(path, "text/html; charset=utf-8");
-}
-
-async function serveJs(path) {
-  return serveFile(path, "text/javascript");
-}
+import { serveFile } from "https://deno.land/std@0.140.0/http/file_server.ts";
 
 async function handleRequest(request) {
   const { pathname } = new URL(request.url);
 
   // Serve shell apps
   if (pathname === "/shell") {
-    return serveHtml("./shell/dist/index.html");
+    return serveFile(request, "./shell/dist/index.html");
   }
 
   // Serve MFE assets and future versioning (better with glob)
   if (pathname.startsWith("/cdn/shell")) {
-    return serveJs(join("./shell/dist", basename(pathname)));
+    return serveFile(request, join("./shell/dist", basename(pathname)));
   }
 
   if (pathname.startsWith("/cdn/remote")) {
-    console.log("path", join("./remote/dist", basename(pathname)));
-
-    return serveJs(join("./remote/dist", basename(pathname)));
+    return serveFile(request, join("./remote/dist", basename(pathname)));
   }
 }
 
