f3c7d7d6b2cf8c006d1dc5e0d3922195c125e273
Justin Awrey <awreyjustin@gmail.com>
2022-05-25 15:29:39 -0700

    ok

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import { basename, join } from "https://deno.land/std@0.140.0/path/mod.ts";
-import { serveFile } from "https://deno.land/std@0.140.0/http/file_server.ts";
+import {
+  serveFile,
+  serveDir,
+} from "https://deno.land/std@0.140.0/http/file_server.ts";
 
 async function handleRequest(request) {
   const { pathname } = new URL(request.url);
 
   // Serve shell apps
   if (pathname === "/shell") {
-    return serveFile(request, "./shell/dist/index.html");
+    return serveFile(request, "shell/dist/index.html");
   }
 
-  // Serve MFE assets and future versioning (better with glob)
+  // Serve MFE assets and future versioning
   if (pathname.startsWith("/cdn/shell")) {
-    return serveFile(request, join("./shell/dist", basename(pathname)));
+    return serveDir(request, {
+      fsRoot: "shell/dist",
+    });
   }
 
   if (pathname.startsWith("/cdn/remote")) {
-    return serveFile(request, join("./remote/dist", basename(pathname)));
+    return serveDir(request, {
+      fsRoot: "remote/dist",
+    });
   }
+
+  return new Response("invalid path");
 }
 
 serve(handleRequest);
